9ba6f5c2a6f76e483076c08365b9f7a6ede47ab0
Gordon Mancuso <731698+Tobithy@users.noreply.github.com>
2020-01-31 21:09:41 -0700

    debug and rework db tables, add some data

--- db/db_setup2.sql
+++ db/db_setup2.sql
@@ -9,12 +9,12 @@ CREATE TABLE user_account (
     hashed_password         varchar(255)    NOT NULL        ,
     first_name              varchar(255)                    ,
     last_name               varchar(255)                    ,
-    data_display_pref_id    integer         REFERENCES data_display_pref (data_display_pref_id)
+    data_display_pref_id    integer         REFERENCES data_display_pref (data_display_pref_id) NOT NULL
 );
 
 CREATE TABLE clinical_test (
     clinical_test_id            serial          PRIMARY KEY     ,
-    clinical_test_short_name    varchar(16)     NOT NULL UNIQUE ,
+    clinical_test_label         varchar(16)     NOT NULL UNIQUE ,
     clinical_test_name          varchar(255)    NOT NULL UNIQUE ,
     clinical_test_description   varchar(1024)                   ,
     clinical_test_format        varchar(16)                     ,
@@ -23,12 +23,11 @@ CREATE TABLE clinical_test (
 
 CREATE TABLE clinical_data (
     clinical_data_id        serial          PRIMARY KEY     ,
-    user_account_id         integer         REFERENCES user_account (user_account_id),
+    user_account_id         integer         REFERENCES user_account (user_account_id) NOT NULL,
     data_date               date            NOT NULL        ,
-    clinical_test_id        integer         REFERENCES clinical_test (clinical_test_id),
+    clinical_test_id        integer         REFERENCES clinical_test (clinical_test_id) NOT NULL,
     data_float              real                            ,
     data_text               varchar(255)                    ,
     data_comment            varchar(1024)                   ,
     CHECK (data_float IS NOT NULL OR data_text IS NOT NULL)
 );
-
